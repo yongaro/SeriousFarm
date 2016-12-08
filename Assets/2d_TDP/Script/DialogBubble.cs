72bcee95b88d988b29905882ae8c40659f876e62
qphilippot <quentin.philippot@gmail.com>
2016-12-08 22:50:23 +0100

    Texte rempli pour tous les mois

--- Assets/2d_TDP/Script/DialogBubble.cs
+++ Assets/2d_TDP/Script/DialogBubble.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using AssemblyCSharp;
@@ -237,7 +237,6 @@ public class DialogBubble : MonoBehaviour {
 			maiDialogue.Add("D'Avril à Juin, c'est la saison idéale pour semer du mais !");
 			maiDialogue.Add("De Mars à Août, c'est la saison idéale pour semer des navets !");
 			
-			maiDialogue.Add("Avril, c'est le dernier mois pour semer des aubergines. Semez-vite si ce n'est pas encore fait !");
 			maiDialogue.Add("Si vous avez semé des patates dans les temps, elles ne devraient plus tarder à être prêtes !");
 			maiDialogue.Add("C'est le dernier mois pour semer des poivrons, c'est maintenant ou jamais !");
 			
@@ -256,9 +255,13 @@ public class DialogBubble : MonoBehaviour {
 			juinDialogue.Add("D'Avril à Juin, c'est la saison idéale pour semer du mais !");
 			juinDialogue.Add("De Mars à Août, c'est la saison idéale pour semer des navets !");
 			
-			juinDialogue.Add("Avril, c'est le dernier mois pour semer des aubergines. Semez-vite si ce n'est pas encore fait !");
-			juinDialogue.Add("Si vous avez semé des patates dans les temps, elles ne devraient plus tarder à être prêtes !");
-			juinDialogue.Add("C'est le dernier mois pour semer des poivrons, c'est maintenant ou jamais !");
+
+			juinDialogue.Add("De Juin à Septembre, c'est la saison des aubergines ! Si vous les avez semé à temps, c'est l'heure de la récolte, miam !");
+			juinDialogue.Add("De Juin à Aout, c'est la moisson. Si vous les avez semé du blé à temps, c'est l'heure de la récolte !");
+			juinDialogue.Add("C'est le dernier mois pour semer des choux fleur, c'est maintenant ou jamais !");
+			juinDialogue.Add("C'est le dernier mois pour semer des choux, c'est maintenant ou jamais !");
+			juinDialogue.Add("C'est le dernier mois pour semer des citrouilles, c'est maintenant ou jamais !");
+			juinDialogue.Add("C'est le dernier mois pour semer du mais, c'est maintenant ou jamais !");
 			
 
 			juinDialogue.Add("Bip bip ...");
@@ -266,21 +269,93 @@ public class DialogBubble : MonoBehaviour {
 			break;
 			
 			case 6:
+			List<string> juilletDialogue = new List<string>();
+			juilletDialogue.Add("Bip bip, d'après des les forums de jardiniers, le comcombre se sème de Mars jusqu'à début Juillet et peut être récolté jusqu'en Novembre.");
+			juilletDialogue.Add("De Mars à Août, c'est la saison idéale pour semer des navets !");
+			
+
+			juilletDialogue.Add("De Juin à Septembre, c'est la saison des aubergines ! Si vous les avez semé à temps, c'est l'heure de la récolte, miam !");
+			juilletDialogue.Add("De Juin à Aout, c'est la moisson. Si vous les avez semé du blé à temps, c'est l'heure de la récolte !");
+		
+			juilletDialogue.Add("C'est le dernier mois pour semer des graines de concombres, c'est maintenant ou jamais !");
+			juilletDialogue.Add("La saison de consommation des concombres commence, recoltez-les et tirez en le meilleur prix chez le commerçant !");
+			
+
+			juilletDialogue.Add("Bip bip ...");
+			return juilletDialogue[Random.Range(0, juilletDialogue.Count - 1)];
+			
 			break;
 			
 			case 7:
+
+			List<string> aoutDialogue = new List<string>();
+			aoutDialogue.Add("On est en Août, c'est le dernier mois pour semer des navets !");
+			
+
+			aoutDialogue.Add("De Juin à Septembre, c'est la saison des aubergines ! Si vous les avez semé à temps, c'est l'heure de la récolte, miam !");
+			aoutDialogue.Add("Bip bip, dernier mois pour récoltez le blé et le mais que vous avez semés.");
+		
+			aoutDialogue.Add("C'est le début des récoltes des choux fleur, arrêtez de semez et récoltez les jusqu'en Octobre.");
+			
+			aoutDialogue.Add("Vive l'été, en août récoltez les légumes du soleil : aubergines, tomates, poivrons et courgette !");
+			aoutDialogue.Add("En auot, début des récoltes d'oignons, bip");
+			aoutDialogue.Add("Bip bip ...");
+
+			return aoutDialogue[Random.Range(0, aoutDialogue.Count - 1)];
 			break;
 			
 			case 8:
+
+			List<string> septembreDialogue = new List<string>();
+			septembreDialogue.Add("Septembre, c'est le dernier mois pour récolter des aubergines !");
+			
+
+			septembreDialogue.Add("De Septembre à Novembre, c'est la saison pour semer du blé");
+			septembreDialogue.Add("Bip bip, dernier mois pour semer des carottes");
+			septembreDialogue.Add("Bip bip, dernier mois pour récolter des tomates");
+			septembreDialogue.Add("De Septembre à Novembre, c'est la saison des navets, récoltez les et vendez le pour en tirer le meilleur prix");
+		
+			septembreDialogue.Add("Bip bip ...");
+			
+			return septembreDialogue[Random.Range(0, septembreDialogue.Count - 1)];
 			break;
 			
 			case 9:
+
+			List<string> octobreDialogue = new List<string>();
+			octobreDialogue.Add("Octobre, c'est le dernier mois pour récolter des poivrons !");
+			octobreDialogue.Add("Octobre, c'est le dernier mois pour récolter des patates !");
+			octobreDialogue.Add("De Septembre à Novembre, c'est la saison pour récolter des navets");
+			
+			octobreDialogue.Add("Bip bip, dernier mois pour semer des fraises");
+			octobreDialogue.Add("Bip bip, dernier mois pour récolter des choux fleur");
+			octobreDialogue.Add("De Septembre à Novembre, c'est la saison des navets, récoltez les et vendez le pour en tirer le meilleur prix");
+		
+			octobreDialogue.Add("Bip bip ...");
+			
+			return octobreDialogue[Random.Range(0, octobreDialogue.Count - 1)];
 			break;
 			
 			case 10:
+
+			List<string> novembreDialogue = new List<string>();
+			novembreDialogue.Add("Novembre, c'est le dernier mois pour semer du blé !");
+			novembreDialogue.Add("Novembre, c'est le dernier mois pour récolter du concombres !");
+			novembreDialogue.Add("De Novembre à Janvier, c'est la saison pour récolter des carottes");
+
+			novembreDialogue.Add("Bip bip ...");
+			
+			return novembreDialogue[Random.Range(0, novembreDialogue.Count - 1)];
 			break;
 
 			case 11:
+
+			List<string> decembreDialogue = new List<string>();
+			decembreDialogue.Add("De Novembre à Janvier, c'est la saison pour récolter des carottes");
+		
+			decembreDialogue.Add("Bip bip ...");
+			
+			return decembreDialogue[Random.Range(0, decembreDialogue.Count - 1)];
 			break;
 		}
 
