48e4369444a17dcaefd98c0d90410493d78fb6fd
yongaro <resodafk@gmail.com>
2017-01-01 23:00:43 +0100

    fix meteo et modification volume des bruits de pas(herbe)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,22 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour
-{
-
+public class CameraController : MonoBehaviour{
     public GameObject player;
     private Vector3 offset;
+	public GameObject rainParticles;
+	public GameObject snowParticles;
+	private Vector3 particlesOffset;
+	
 
-
-    void Start()
-    {
+    void Start(){
         offset = transform.position - player.transform.position;
+		rainParticles = GameObject.Find("Pluie");
+		snowParticles = GameObject.Find("Neige2");
+		particlesOffset = new Vector3( 5.0f, 5.0f, 0.0f);
     }
 
-
-
-    void Update()
-    {
+    void Update(){
         transform.position = player.transform.position + offset;
+		if( rainParticles != null ){ rainParticles.transform.position =  transform.position + particlesOffset; }
+		if( snowParticles != null ){ snowParticles.transform.position =  transform.position + particlesOffset; }
     }
 }
